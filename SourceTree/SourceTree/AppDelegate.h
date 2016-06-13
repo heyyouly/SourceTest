5df9ca8e99a26986cf7e94fba412653882e9601c
刘洋 <liuyang@liuyangdeMacBook-Pro.local>
2016-06-14 07:50:44 +0800

    第一次提交

---
+++ SourceTree/SourceTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceTree
+//
+//  Created by 刘洋 on 16/6/14.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
